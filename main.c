27558ba6588ad69add4c56e78700d79ac2c7a138
Olazs <oliverbalazsgal@outlook.com>
2025-03-30 01:53:29 +0100

    unification

--- main.c
+++ main.c
@@ -179,41 +179,41 @@ void updateArrayWithNeighborAverage(float arr[], int size) {
 int main() { //Tesztek:
     int x = 10, y = 20, z = 5;
     float a = 5.5; float b = 6.5;
-    printf("1. A változó értéke: %d\n", getValue(&x));
+    printf("1. A valtozo erteke: %d\n", getValue(&x));
 
-    printf("2. Swap előtt: x = %d, y = %d\n", x, y);
+    printf("2. Swap elott: x = %d, y = %d\n", x, y);
     swap(&x, &y);
-    printf("   Swap után: x = %d, y = %d\n", x, y);
+    printf("   Swap utan: x = %d, y = %d\n", x, y);
 
-    printf("3. Rendezés előtt: x = %d, y = %d, z = %d\n", x, y, z);
+    printf("3. Rendezes elott: x = %d, y = %d, z = %d\n", x, y, z);
     sortThree(&x, &y, &z);
-    printf("   Rendezés után: x = %d, y = %d, z = %d\n", x, y, z);
+    printf("   Rendezes utan: x = %d, y = %d, z = %d\n", x, y, z);
 
     int arr[] = {3, 1, 4, 1, 5, 9, 2, 6};
     int *min, *max;
     findMinMax(arr, 8, &min, &max);
     printf("4. Legkisebb: %d, Legnagyobb: %d\n", *min, *max);
 
-    printf("5. Legkisebb szám: %d\n", minOfThree(&x, &y, &z));
+    printf("5. Legkisebb szam: %d\n", minOfThree(&x, &y, &z));
 
     sortDescending(arr, 8);
-    printf("6. Csökkenő sorrend: ");
+    printf("6. Csokkeno sorrend: ");
     for (int i = 0; i < 8; i++) printf("%d ", arr[i]);
     printf("\n");
 
     shuffleArray(arr, 8);
-    printf("7. Összekevert tömb: ");
+    printf("7. osszekevert tomb: ");
     for (int i = 0; i < 8; i++) printf("%d ", arr[i]);
     printf("\n");
 
     zeroArray(arr, 8);
-    printf("8. Nullázott tömb: ");
+    printf("8. Nullazott tomb: ");
     for (int i = 0; i < 8; i++) printf("%d ", arr[i]);
     printf("\n");
 
     int arr2[] = {1, 2, 3, 4, 5};
     modifyArray(arr2, 5);
-    printf("9. Módosított tömb: ");
+    printf("9. Modositott tomb: ");
     for (int i = 0; i < 5; i++) printf("%d ", arr2[i]);
     printf("\n");
 
@@ -231,34 +231,34 @@ int main() { //Tesztek:
 
     //16:
     int arr16[] = {3, 1, 4, 1, 5, 9, 2, 6};
-    printf("16. Maradék nélkül osztható számok (2-vel): %d\n", countDivisible(arr16, 8, 2));
+    printf("16. Maradek nelkul oszthato szamok (2-vel): %d\n", countDivisible(arr16, 8, 2));
 
     //17:
     int x17 = 8; int y17 = 5;
     int *px17 = &x17, *py17 = &y17;
-    printf("17. A nagyobb szám: %d\n", isBiggerWithPointers(px17, py17));
+    printf("17. A nagyobb szam: %d\n", isBiggerWithPointers(px17, py17));
 
     //18:
     int x18 = 7; int y18 = 6;
     int *px18 = &x18, *py18 = &y18;
-    printf("18. A kissebb szám: %d\n", isSmallerWithPointers(px18, py18));
+    printf("18. A kissebb szam: %d\n", isSmallerWithPointers(px18, py18));
 
     //19:
     float x19=3.5;
     float *px19=&x19;
-    printf("19. A szám négyzete: %.2f\n", toSquare(px19));
+    printf("19. A szam negyzete: %.2f\n", toSquare(px19));
 
-    printf("20. Van-e egymás mellett/alatt azonos elem a mátrixban? %s\n", hasAdjacentEqualElements((int*[]){(int[]){1,2,3}, (int[]){4,5,6}, (int[]){7,8,8}}, 3) ? "IGAZ" : "HAMIS");
+    printf("20. Van-e egymas mellett/alatt azonos elem a matrixban? %s\n", hasAdjacentEqualElements((int*[]){(int[]){1,2,3}, (int[]){4,5,6}, (int[]){7,8,8}}, 3) ? "IGAZ" : "HAMIS");
 
-    printf("21. Hány egymás mellett/alatt azonos elem a mátrixban? %d\n", hasAdjacentEqualElements((int*[]){(int[]){1,2,3}, (int[]){4,5,6}, (int[]){7,8,8}}, 3));
+    printf("21. Hány egymas mellett/alatt azonos elem a matrixban? %d\n", hasAdjacentEqualElements((int*[]){(int[]){1,2,3}, (int[]){4,5,6}, (int[]){7,8,8}}, 3));
 
     //22:
     float arr22[] = {2.3, 3.8, 3.0, 6.5, 8.7};
     updateArrayWithNeighborAverage(arr22, 5);
-    printf("22. Frissített tömb: ");
+    printf("22. Frissitett tomb: ");
     for (int i = 0; i < 5; i++) printf("%.2f ", arr22[i]);
     printf("\n");
 
 
-    return 0;
+    return 100;
 }
